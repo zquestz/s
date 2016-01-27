60b4188c742053aaa022f7eae5d8bc330c1baa09
mickythump <mikolaj06120@gmail.com>
2016-01-27 20:50:24 +0100

    fix comments

--- providers/atmospherejs/atmospherejs.go
+++ providers/atmospherejs/atmospherejs.go
@@ -11,11 +11,11 @@ func init() {
 	providers.AddProvider("atmospherejs", &AtmosphereJSProvider{})
 }
 
-// BaiduProvider adheres to the Provider interface.
+// AtmosphereJSProvider adheres to the Provider interface.
 type AtmosphereJSProvider struct {
 }
 
-// BuildURI generates a search URL for Google.
+// BuildURI generates a search URL for AtmosphereJS.
 func (p *AtmosphereJSProvider) BuildURI(q string) string {
 	return fmt.Sprintf("https://atmospherejs.com/?q=%s", url.QueryEscape(q))
 }
