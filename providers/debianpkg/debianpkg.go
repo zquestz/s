79e080e1a82d1578d402e656966a178d3d9d7958
Jean Kahrs <kahrs@twooit.de>
2016-02-10 09:58:58 +0100

    add debian package search #50

--- providers/debianpkg/debianpkg.go
+++ providers/debianpkg/debianpkg.go
@@ -16,6 +16,6 @@ type Provider struct {
 }
 
 // BuildURI generates the search URL for packages.debian.org.
-func (p *DebianPKGProvider) BuildURI(q string) string {
+func (p *Provider) BuildURI(q string) string {
 	return fmt.Sprintf("https://packages.debian.org/search?keywords=%s&searchon=names&suite=stable&section=all", url.QueryEscape(q))
 }
