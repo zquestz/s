a4a83a9993d49d909a4e44665411bb92004ee957
quest <quest@mac.com>
2016-02-20 00:54:03 -0800

    Added debianpkg provider

---
+++ providers/debianpkg/debianpkg.go
@@ -0,0 +1,20 @@
+package debianpkg
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/zquestz/s/providers"
+)
+
+func init() {
+	providers.AddProvider("debianpkg", &Provider{})
+}
+
+// Provider merely implements the Provider interface.
+type Provider struct{}
+
+// BuildURI generates a search URL for debian.org packages.
+func (p *Provider) BuildURI(q string) string {
+	return fmt.Sprintf("https://packages.debian.org/search?keywords=%s&searchon=names&suite=stable&section=all", url.QueryEscape(q))
+}
