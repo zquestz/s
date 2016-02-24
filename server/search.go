7b159a1755771fd640637f5a86fa0dce7a0e1674
Pratyush <pratyushverma@outlook.com>
2016-02-24 10:42:50 +0530

    Use locale from accept headers

--- server/search.go
+++ server/search.go
@@ -3,12 +3,20 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/zquestz/s/providers"
+	"golang.org/x/text/language"
 )
 
 func search(defaultProvider string, verbose bool, w http.ResponseWriter, r *http.Request) {
 	requestedProvider := r.FormValue("provider")
+	acceptLanguageHeader := r.Header.Get("Accept-Language")
+
+	if strings.TrimSpace(acceptLanguageHeader) != "" {
+		locale := parseAcceptLanguage(acceptLanguageHeader)
+		providers.SetClientLocale(locale)
+	}
 	if requestedProvider == "" {
 		requestedProvider = defaultProvider
 	}
@@ -39,4 +47,13 @@ func providerNotFound(provider string, w http.ResponseWriter, r *http.Request) {
 
 func queryNotFound(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, fmt.Sprintf("A search query is required."), 400)
+}
+
+func parseAcceptLanguage(header string) string {
+	tags, _, err := language.ParseAcceptLanguage(header)
+	if err != nil {
+		return ""
+	}
+	// Using the first Tag as Tags are sorted by highest weight first
+	return tags[0].String()
 }
