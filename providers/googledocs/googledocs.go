946599ab059b2bdb025ea70fe478a9c3aecc370c
Dennis Rowe <dennis.rowe@apcera.com>
2016-03-22 15:55:58 -0700

    Add a googledocs provider

---
+++ providers/googledocs/googledocs.go
@@ -0,0 +1,20 @@
+package googledocs
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/zquestz/s/providers"
+)
+
+func init() {
+	providers.AddProvider("googledocs", &Provider{})
+}
+
+// Provider merely implements the Provider interface.
+type Provider struct{}
+
+// BuildURI generates a search URL for Google Docs.
+func (p *Provider) BuildURI(q string) string {
+	return fmt.Sprintf("https://docs.google.com/document/u/0/?q=%s", url.QueryEscape(q))
+}
