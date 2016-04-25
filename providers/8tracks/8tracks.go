2be791bc08ac89e892d98a4447268b9e0c9f9403
Josh Ellithorpe <quest@mac.com>
2016-04-24 17:21:05 -0700

    Fix 8tracks search to be more robust

--- providers/8tracks/8tracks.go
+++ providers/8tracks/8tracks.go
@@ -16,7 +16,7 @@ type Provider struct{}
 
 // BuildURI generates a search URL for 8tracks.
 func (p *Provider) BuildURI(q string) string {
-	return fmt.Sprintf("https://8tracks.com/explore/%s", url.QueryEscape(q))
+	return fmt.Sprintf("https://8tracks.com/search?q=%s", url.QueryEscape(q))
 }
 
 // Tags returns the tags relevant to this provider.
