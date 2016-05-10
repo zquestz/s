d2411d749a1a9abf7674fe9e8cef220f1e5432f1
Josh Ellithorpe <quest@mac.com>
2016-05-09 18:55:09 -0700

    Added crates to search for rust libs

---
+++ providers/crates/crates.go
@@ -0,0 +1,25 @@
+package crates
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/zquestz/s/providers"
+)
+
+func init() {
+	providers.AddProvider("crates", &Provider{})
+}
+
+// Provider merely implements the Provider interface.
+type Provider struct{}
+
+// BuildURI generates a search URL for crates.
+func (p *Provider) BuildURI(q string) string {
+	return fmt.Sprintf("https://crates.io/search?q=%s", url.QueryEscape(q))
+}
+
+// Tags returns the tags relevant to this provider.
+func (p *Provider) Tags() []string {
+	return []string{}
+}
