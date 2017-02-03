eb986463cda74119dcbacf5df0672f36c4acf45c
Josh Ellithorpe <quest@mac.com>
2017-02-02 23:49:22 -0800

    Fix zhihu tags

--- providers/zhihu/zhihu.go
+++ providers/zhihu/zhihu.go
@@ -21,5 +21,10 @@ func (p *Provider) BuildURI(q string) string {
 
 // Tags returns the tags relevant to this provider.
 func (p *Provider) Tags() []string {
-	return []string{"forums"}
+	switch providers.Language() {
+	case "zh":
+		return []string{"forums"}
+	default:
+		return []string{}
+	}
 }
