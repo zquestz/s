83e0d269b71a0e1206499af37369218f34d7835d
Timo Pallach <timo@pallach.de>
2021-11-29 03:09:47 +0100

    The Wikipedia provider was extended to support different regions.

--- providers/wikipedia/wikipedia.go
+++ providers/wikipedia/wikipedia.go
@@ -16,7 +16,26 @@ type Provider struct{}
 
 // BuildURI generates a search URL for Wikipedia.
 func (p *Provider) BuildURI(q string) string {
-	return fmt.Sprintf("https://en.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	switch providers.Region() {
+	case "BR":
+		return fmt.Sprintf("https://br.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "CA":
+		return fmt.Sprintf("https://ca.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "DE":
+		return fmt.Sprintf("https://de.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "ES":
+		return fmt.Sprintf("https://es.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "FR":
+		return fmt.Sprintf("https://fr.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "IT":
+		return fmt.Sprintf("https://it.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "JP":
+		return fmt.Sprintf("https://jp.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	case "NL":
+		return fmt.Sprintf("https://nl.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	default:
+		return fmt.Sprintf("https://en.wikipedia.org/wiki/Special:Search?search=%s", url.QueryEscape(q))
+	}
 }
 
 // Tags returns the tags relevant to this provider.
