7664277bcedab687d1bdea7ac583b5d8eea8f107
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2023-11-27 09:39:36 -0500

    Tag "twitchtv" provider with "games"

--- providers/twitchtv/twitchtv.go
+++ providers/twitchtv/twitchtv.go
@@ -21,5 +21,5 @@ func (p *Provider) BuildURI(q string) string {
 
 // Tags returns the tags relevant to this provider.
 func (p *Provider) Tags() []string {
-	return []string{}
+	return []string{"games"}
 }
